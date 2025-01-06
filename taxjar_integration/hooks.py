55e78823cd259d6d811482574d068c6b6cdac283
Brett <brettney40hands@gmail.com>
2025-01-06 20:48:32 +0000

    bugfix; app bump
    malformed hook = impossible to delete invoices; app updated to remove delete hook

--- taxjar_integration/hooks.py
+++ taxjar_integration/hooks.py
@@ -106,7 +106,7 @@ doc_events = {
 	"Sales Invoice": {
 		"on_submit": "taxjar_integration.taxjar_integration.taxjar_integration.create_transaction",
 		"on_cancel": "taxjar_integration.taxjar_integration.taxjar_integration.delete_transaction",
-		"on_trash": "method"
+		# "on_trash": "method"
 	},
 	("Quotation", "Sales Order", "Sales Invoice"): {
 		"validate": ["taxjar_integration.taxjar_integration.taxjar_integration.set_sales_tax"]
